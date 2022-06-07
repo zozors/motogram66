f62eaab84fd2c1e05005267ccabf777d0bda1ba4
zozors <romance.dawn1989@gmail.com>
2022-06-07 23:03:46 +0900

    修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,7 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
-  validates :content, presence: true
-  
   belongs_to :user
   has_many :comments
-  has_one_attached :image
 
   def self.search(search)
     if search != ""
