82ea1f21a6125e98fbe39955d828d6abfd0f8f1e
zozors <romance.dawn1989@gmail.com>
2022-06-07 18:25:35 +0900

    Active strage導入

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,10 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
+  validates :content, presence: true
+  
   belongs_to :user
   has_many :comments
+  has_one_attached :image
 
   def self.search(search)
     if search != ""
